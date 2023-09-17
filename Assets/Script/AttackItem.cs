27efe566f538b2ff18b275cfc7bc826180728bf9
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-09-18 03:13:22 +0700

    A little bit ocp

---
+++ Assets/Script/AttackItem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackItem : CategoryAb
+{
+    
+    public override void SetCategoryIndex(GameObject Categoryname)
+    {
+      // if()
+      // {
+
+      // }
+    }
+}
